5ba65fb2b8ad7ef6d261965bdb2213e3f11bb9e8
Brian Olore <brian@olore.net>
2020-09-18 08:19:05 -0400

    1st and 2nd col are always findable, but 3rd and 4th never are

--- src/App.test.js
+++ src/App.test.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import GridExample from './GridExample';
 
-// THIS FAILS
-// Unable to find an element with the text: Ford
-test('renders field for make', () => {
-  const { getByText } = render(<GridExample />);
-  expect(getByText("Ford")).toBeInTheDocument();
-});
+describe('the tests', () => {
 
-// THIS FAILS
-// Unable to find an element with the text: 72000
-test('renders field for price', () => {
-  const { getByText } = render(<GridExample />);
-  expect(getByText("72000")).toBeInTheDocument();
-});
+  test('renders field for make', () => {
+    const { getByText } = render(<GridExample />);
+    expect(getByText("Ford")).toBeInTheDocument();
+  });
 
+  test('renders field for price', () => {
+    const { getByText } = render(<GridExample />);
+    expect(getByText("72000")).toBeInTheDocument();
+  });
 
-// THIS PASSES 
-test('renders field for model', () => {
-  const { getByText } = render(<GridExample />);
-  expect(getByText("Boxter")).toBeInTheDocument();
-});
+  test('renders field for model', () => {
+    const { getByText } = render(<GridExample />);
+    expect(getByText("Boxter")).toBeInTheDocument();
+  });
+
+  // test('renders field for color', () => {
+  //   const { getByText } = render(<GridExample />);
+  //   expect(getByText("blue")).toBeInTheDocument();
+  // });
+
+})
--- src/GridExample.js
+++ src/GridExample.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import {AgGridReact} from 'ag-grid-react';
+import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-enterprise';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const GridExample = () => {
-    const rowData = () => [
-        {make: "Toyota", model: "Celica", price: 35000},
-        {make: "Ford", model: "Mondeo", price: 32000},
-        {make: "Porsche", model: "Boxter", price: 72000}
-    ];
+  const rowData = () => [
+    { make: "Toyota", model: "Celica", price: 35000, color: "blue" },
+    { make: "Ford", model: "Mondeo", price: 32000, color: "red" },
+    { make: "Porsche", model: "Boxter", price: 72000, color: "black" }
+  ];
 
-    const columnDefs = [{
-      cellRenderer: "agGroupCellRenderer",
-      field: "make"
-    }, {
-      field: "model"
-    }, {
-      field: "price"
-    }];
+  // It seems only the first 2 cols are findable
+  // no matter how I change the fields, looking for the 3rd/4th in a test fails
+  const columnDefs = [{
+    field: "model"        // This field is findable
+  }, {
+    field: "price"        // This field is findable
+  }, {
+    field: "make"         // This field is NOT findable
+  // }, {
+  //   field: "color"        // This field is NOT findable
+  }];
 
-    return (
+  return (
 
-        <div className="ag-theme-alpine" style={ {height: '200px', width: '600px'} }>
-            <AgGridReact
-                columnDefs={columnDefs}
-                rowData={rowData()}
-                >
-            </AgGridReact>
-        </div>
-    );
+    <div className="ag-theme-alpine" style={{ height: '300px', width: '800px' }}>
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowData={rowData()}
+        pagination={true}
+      >
+      </AgGridReact>
+    </div>
+  );
 };
 
 export default GridExample;
