118756120b125460f470cc916096871a3cc4bb22
Brian Olore <brian@olore.net>
2020-09-18 09:15:48 -0400

    noticed that not all the headers render either... first 2 still good, others nope

--- src/App.test.js
+++ src/App.test.js
@@ -2,26 +2,38 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import GridExample from './GridExample';
 
-describe('the tests', () => {
+describe("the tests", () => {
 
-  test('renders field for make', async () => {
-    const { getByText } = render(<GridExample />);
-    expect(getByText("Ford")).toBeInTheDocument();
+  describe("headers", () => {
+    test('renders all the headers', async () => {
+      const { getByText } = render(<GridExample />);
+      expect(getByText("Make")).toBeInTheDocument();
+      expect(getByText("Model")).toBeInTheDocument();
+      expect(getByText("Price")).toBeInTheDocument();
+      expect(getByText("Color")).toBeInTheDocument();
+    });
   });
 
-  test('renders field for price', async () => {
-    const { getByText } = render(<GridExample />);
-    expect(getByText("72000")).toBeInTheDocument();
-  });
+  describe("data", () => {
+    test('renders field for make', async () => {
+      const { getByText } = render(<GridExample />);
+      expect(getByText("Toyota")).toBeInTheDocument();
+    });
 
-  test('renders field for model', async () => {
-    const { getByText } = render(<GridExample />);
-    expect(getByText("Boxter")).toBeInTheDocument();
-  });
+    test('renders field for model', async () => {
+      const { getByText } = render(<GridExample />);
+      expect(getByText("Celica")).toBeInTheDocument();
+    });
 
-  // test('renders field for color', () => {
-  //   const { getByText } = render(<GridExample />);
-  //   expect(getByText("blue")).toBeInTheDocument();
-  // });
+    test('renders field for price', async () => {
+      const { getByText } = render(<GridExample />);
+      expect(getByText("35000")).toBeInTheDocument();
+    });
+
+    test('renders field for color', () => {
+      const { getByText } = render(<GridExample />);
+      expect(getByText("blue")).toBeInTheDocument();
+    });
+  });
 
 })
--- src/GridExample.js
+++ src/GridExample.js
@@ -5,36 +5,34 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const GridExample = () => {
-  const rowData = () => [
-    { make: "Toyota", model: "Celica", price: 35000, color: "blue" },
-    { make: "Ford", model: "Mondeo", price: 32000, color: "red" },
-    { make: "Porsche", model: "Boxter", price: 72000, color: "black" },
-    { make: "Volvo", model: "Wagon", price: 172000, color: "pink" }
-  ];
 
-  // It seems only the first 2 cols are findable
-  // no matter how I change the fields, 
-  // looking for the 3rd/4th in a test fails
-  const columnDefs = [{
-    field: "make"        // This field is findable
-  }, {
-    field: "model"        // This field is findable
-  }, {
-    field: "price"         // This field is NOT findable
-  // }, {
-  //   field: "color"        // This field is NOT findable
-  }];
+  // NOTE: adding `cellRenderer: "agGroupCellRenderer",` 
+  //       to any field makes it not findable
 
   return (
 
     <div className="ag-theme-alpine" style={{ height: '300px', width: '800px' }}>
       <AgGridReact
-        columnDefs={columnDefs}
-        rowData={rowData()}
+        columnDefs={ /* First 2 always findable, others never */
+          [
+            { field: "make" }, 
+            { field: "model" }, 
+            { field: "price" }, 
+            { field: "color" },
+          ]
+        }
+        rowData={
+          [{ 
+            make: "Toyota", 
+            model: "Celica",
+            price: "35000", 
+            color: "blue" 
+          }]
+        }
         pagination={true}
       >
       </AgGridReact>
-    </div>
+    </div >
   );
 };
 
